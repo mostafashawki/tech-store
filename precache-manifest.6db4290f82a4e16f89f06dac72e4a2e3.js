ad27acd88787f79b0d99abf53336640b47cb6d00
Mostafa Shawki <mostafashawki@Mostafas-MacBook-Pro.local>
2020-04-13 16:07:01 +0200

    Updates

---
+++ precache-manifest.6db4290f82a4e16f89f06dac72e4a2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b7bd9c6c3c7e29558ffa20e734a221",
+    "url": "/tech-store/index.html"
+  },
+  {
+    "revision": "4a4cca14c7789e9d2509",
+    "url": "/tech-store/static/css/main.080ed009.chunk.css"
+  },
+  {
+    "revision": "2e2c6db266f214135bce",
+    "url": "/tech-store/static/js/2.53830999.chunk.js"
+  },
+  {
+    "revision": "f2eabfef24c7bc3e284c24358f63f959",
+    "url": "/tech-store/static/js/2.53830999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4cca14c7789e9d2509",
+    "url": "/tech-store/static/js/main.45ef1558.chunk.js"
+  },
+  {
+    "revision": "7ad09240bc35fbe6d219",
+    "url": "/tech-store/static/js/runtime-main.5cf4555c.js"
+  }
+]);
